7efc15de45fbc705e1d368fc2fe8bf8103843f71
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-10-03 13:06:20 +0900

    Fix issue with image name starting with docker.io

--- tools/build.sh
+++ tools/build.sh
@@ -34,10 +34,10 @@ esac
 readonly IMAGE_NAME
 readonly DOCKERFILE
 
-CURRENT_IMAGE="$($DOCKER image ls -q "$IMAGE_NAME":latest)"
+CURRENT_IMAGE="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME":latest || :)"
 readonly CURRENT_IMAGE
 $DOCKER image build -f "$DOCKERFILE" -t "$IMAGE_NAME" .
-LATEST_IMAGE="$($DOCKER image ls -q "$IMAGE_NAME":latest)"
+LATEST_IMAGE="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME":latest || :)"
 readonly LATEST_IMAGE
 if [[ "$CURRENT_IMAGE" != "$LATEST_IMAGE" ]]; then
   $DOCKER image tag "$IMAGE_NAME":latest "$IMAGE_NAME":"$(date +%Y%m%d%H%S)"
